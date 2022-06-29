3428e6e40d16365019acde987080b0f3d9ba739e
Amamason <104336445+Amamason@users.noreply.github.com>
2022-06-29 21:10:27 +1000

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Your code here
+
+class Cat { 
+    constructor(name, sex) {
+    this.name = name;
+    this.sex = sex;
+    }
+speak() {
+   return `${this.name} says meow!`;
+}
+}
+class Dog {
+    constructor(name, sex) {
+        this.name = name;
+        this.sex = sex;
+        }
+speak() {
+    console.log(`${this.name} says woof!`);
+  }
+}
+
+class Bird {
+    constructor(name, sex) {
+        this.name = name;
+        this.sex = sex;
+    } 
+}
+
+let Bird2 = new Bird("harry", "male");
+console.log(Bird2);
+
+Bird2.speak = function(){
+  if (this.sex == "male") 
+  console.log( `It's me! ${this.name}, the parrot!`);
+ else 
+  console.log( `${this.name} says squawk!`);
+}
+  console.log(Bird2.speak());
